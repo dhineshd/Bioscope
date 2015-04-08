b5c7a6c3cb5bc42cd03fba3c357b15987c018556
rohitrnathan <rohitraghunathan@Rohits-MBP.eqr>
2015-04-08 00:35:02 -0700

    Add a camera and set preview display

---
+++ app/src/main/java/com/trioscope/chameleon/com/trioscope/chameleon/camera/CameraPreview.java
@@ -0,0 +1,77 @@
+package com.trioscope.chameleon.com.trioscope.chameleon.camera;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/**
+ * Created by rohitraghunathan on 4/7/15.
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+    private static final String TAG = "CameraPreview";
+
+    private SurfaceHolder surfaceHolder;
+
+    private Camera camera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+
+        this.camera = camera;
+        this.surfaceHolder = getHolder();
+        this.surfaceHolder.addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+            Log.d(TAG, "Preview Display Started");
+        } catch (IOException e) {
+            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (surfaceHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            camera.stopPreview();
+            Log.d(TAG, "Preview Display Stopped");
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+            Log.d(TAG, "Preview Display Started");
+
+        } catch (Exception e) {
+            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    // empty. Take care of releasing the Camera preview in your activity.
+    }
+}
