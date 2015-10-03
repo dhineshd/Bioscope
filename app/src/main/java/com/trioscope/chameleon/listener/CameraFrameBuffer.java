d969f9d337b79bd89bd5fe07e0763633a0d2f6d9
Philip Anderson <panderson160@gmail.com>
2015-10-03 14:00:19 -0700

    Fix concurrent modification exception

--- app/src/main/java/com/trioscope/chameleon/listener/CameraFrameBuffer.java
+++ app/src/main/java/com/trioscope/chameleon/listener/CameraFrameBuffer.java
@@ -3,14 +3,15 @@ package com.trioscope.chameleon.listener;
 import com.trioscope.chameleon.camera.impl.FrameInfo;
 import com.trioscope.chameleon.types.CameraInfo;
 
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by phand on 6/11/15.
  */
 public class CameraFrameBuffer {
-    private volatile Set<CameraFrameAvailableListener> listeners = new HashSet<>();
+    private volatile Set<CameraFrameAvailableListener> listeners = Collections.newSetFromMap(new ConcurrentHashMap<CameraFrameAvailableListener, Boolean>());
 
     public void frameAvailable(CameraInfo cameraInfo, CameraFrameData frameData, FrameInfo frameInfo) {
         for (CameraFrameAvailableListener listener : listeners)
