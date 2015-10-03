249dd57ec8823f8ee3100f4cc5ca985295a91752
rohitrnathan <rohitrnathan@gmail.com>
2015-10-03 00:20:17 -0700

    Fix Google Analytics setup

--- app/src/main/java/com/trioscope/chameleon/activity/MainActivity.java
+++ app/src/main/java/com/trioscope/chameleon/activity/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends EnableForegroundDispatchForNFCMessageActivity 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
