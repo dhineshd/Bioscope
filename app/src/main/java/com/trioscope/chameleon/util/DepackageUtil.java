1eb4ca4aa242e7b5b0a355d5d9d03b6714b7b1df
Dhinesh Dharman <dhinesh.dharman@gmail.com>
2015-10-29 23:59:04 -0700

    Commenting out md5 hash check until we optimize it

--- app/src/main/java/com/trioscope/chameleon/util/DepackageUtil.java
+++ app/src/main/java/com/trioscope/chameleon/util/DepackageUtil.java
@@ -129,17 +129,21 @@ public class DepackageUtil {
         }
 
         if (file.exists()) {
+
+            // Commenting out md5 check until we find a way to speed it up
+            // since it takes a long time to open the app every time
+
+
             // File exists, check the md5 too
-            String md5sum = getMd5Sum(file);
-
-            if (md5sum.equals(asset.getExpectedMd5())) {
-                log.info("Md5sum {} matches for file {}", md5sum, file);
-                return true;
-            } else {
-                log.info("Md5sum {} doesnt match {} for file {}", md5sum, asset.getExpectedMd5(), file);
-                return false;
-            }
-
+//            String md5sum = getMd5Sum(file);
+//            if (md5sum.equals(asset.getExpectedMd5())) {
+//                log.info("Md5sum {} matches for file {}", md5sum, file);
+//                return true;
+//            } else {
+//                log.info("Md5sum {} doesnt match {} for file {}", md5sum, asset.getExpectedMd5(), file);
+//                return false;
+//            }
+            return true;
         } else {
             log.info("File doesnt exist, so it has not been downloaded");
             return false;
