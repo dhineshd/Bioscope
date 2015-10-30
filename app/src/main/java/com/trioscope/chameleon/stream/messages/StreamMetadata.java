40f1e31ddb87840fd441116851283d04e89c0b7a
Dhinesh Dharman <dhinesh.dharman@gmail.com>
2015-10-29 22:54:18 -0700

    Using orientation degrees from camera characteristics

--- app/src/main/java/com/trioscope/chameleon/stream/messages/StreamMetadata.java
+++ app/src/main/java/com/trioscope/chameleon/stream/messages/StreamMetadata.java
@@ -2,13 +2,16 @@ package com.trioscope.chameleon.stream.messages;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 /**
  * Created by dhinesh.dharman on 9/23/15.
  */
 @Builder
 @Getter
+@ToString
 public class StreamMetadata {
     // mirror image
     private boolean horizontallyFlipped;
+    private int orientationDegrees;
 }
